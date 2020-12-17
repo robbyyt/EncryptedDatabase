46ef0d472f75ad330df535df830ab4d633020149
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-12-18 00:09:23 +0200

    "add" functionality implemented

--- database/connection.py
+++ database/connection.py
@@ -15,10 +15,10 @@ class DBConnection:
         return self.file_collection.insert_one(file).inserted_id
 
     def delete_one_file(self, file_id):
-        return self.file_collection.delete_one({'_id': ObjectId(file_id)}).deleted_count
+        return self.file_collection.delete_one({'uid': file_id}).deleted_count
 
     def get_file_by_id(self, file_id):
-        found = self.file_collection.find({'_id': ObjectId(file_id)})
+        found = self.file_collection.find({'uid': file_id})
         documents = []
         for doc in found:
             documents.append(doc)
